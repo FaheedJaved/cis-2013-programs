891afd375f011968da9afd68d8389958399e5eb3
FaheedJaved <54917328+FaheedJaved@users.noreply.github.com>
2019-10-09 18:40:25 -0500

    Add files via upload

--- Grade2.js
+++ Grade2.js
@@ -5,37 +5,67 @@ var $ = function (id)
 
 var calculate_click = function () 
 {
-    var floatHwPts, floatMidPts, floatFinPts,stringFinalGrade, intGradeOption, floatTotalPts;
+    var floatHwPts, floatMidPts, floatFinPts, floatTotalPts, intGradeOption, stringFinalGrade;
 
     floatHwPts = parseFloat($("hw_pts").value);
-    floatMidPts= parseFloat($("mid_pts").value);
-    floatFinPts= parseFloat ($("fin_pts").value);
-    floatTotalPts= parseFloat (floatHwPts + floatMidPts + floatFinPts);
-    intGradeOption = parseInt ($("grade_option").value);
-    
-if (intGradeOption === 1) {
-    if (floatTotalPts >= 80)
-        stringFinalGrade = "Pass";
-    else
-        stringFinalGrade = "Fail";
-}
-if (intGradeOption === 2) {
-    if (floatTotalPts >= 90)
-        stringFinalGrade = "A";
-    else if (floatTotalPts >= 80)
+    floatMidPts = parseFloat($("mid_pts").value);
+    floatFinPts = parseFloat($("fin_pts").value);
+    floatTotalPts = parseFloat(floatHwPts+floatMidPts+floatFinPts);
+    intGradeOption = parseInt($("grade_option").value);
+    if (intGradeOption===1)
+{
+    if(floatTotalPts>= 80)
+    {
+        stringFinalGrade ="Pass";
+    }
+ else
+ { 
+    stringFinalGrade = "Fail";
+    }
+  }
+else
+{
+    if(floatTotalPts >= 90){
+    stringFinalGrade = "A";
+    }
+else
+{
+    if(floatTotalPts >= 80){
         stringFinalGrade = "B";
-    else if (floatTotalPts >= 70)
+    }
+else
+{
+    if(floatTotalPts >= 70){
         stringFinalGrade = "C";
-    else if (floatTotalPts >= 60)
-        stringFinalGrade = "D";
-    else
-        stringFinalGrade = "F";
-}
-$("final_grade").value= stringFinalGrade;
+    }
 
+else
+{
+    if(floatTotalPts >= 60){
+        stringFinalGrade = "D"; 
+    }
+
+
+else
+{
+    if(floatTotalPts >= 50){
+       stringFinalGrade ="F";
+    }
+else
+{
+    if(floatTotalPts <=0){
+        stringFinalGrade ="F";
+    }
+}
+}
+}
+}
+}
+}
+    $("final_grade").value =stringFinalGrade;
 
 };
- 
+
 window.onload = function () 
 {
     $("final_grade").value = ""; //blanks the final grade text box upon page load
